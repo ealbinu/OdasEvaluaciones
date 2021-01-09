9f287eed88fb1790c45083d8b4cbaf104347e4e8
Albin Rdz <ealbinu@gmail.com>
2021-01-08 18:43:45 -0600

    fix

--- assets/modulos/relational.js
+++ assets/modulos/relational.js
@@ -6,15 +6,15 @@ var epDrag = {
 }
 var epTarget = {
     isSource:false, isTarget:true,
-    paintStyle: { stroke:'#fff', strokeWidth:6, fill: '#70BF44' },
+    paintStyle: { stroke:'#F88E26', strokeWidth:6, fill: '#fff' },
     maxConnections: -1
 } 
 
 Vue.component('relational', {
-    props: ['value', 'sources', 'targets', 'connections'],
+    props: ['value', 'sources', 'targets', 'connections', 'oks'],
     data() {
         return {
-            status: '',
+            status: [],
             evaluate: false,
             result: false,
             started: false
@@ -23,13 +23,13 @@ Vue.component('relational', {
     computed:{
         
     },
+    /*
     watch: {
         value () {
-            if(this.watch){
-                this.status = this.value
-            }
+            this.status = this.value
         }
     },
+    */
     methods: {
         startConnections () {
             this.started = true
@@ -39,31 +39,63 @@ Vue.component('relational', {
             for (item in this.targets) {
                 jsPlumb.addEndpoint('t_'+item, { anchor:"Left", uuid: 't_'+item }, epTarget )
             }
-            for (item in this.connections) {
-                jsPlumb.connect({ uuids:connections[item] })
+            
+            // Load connections
+            if(this.value){
+                this.status = this.value
+            }
+            for (item in this.status) {
+                jsPlumb.connect({ uuids:this.status[item] })
+            }
+        },
+        verify () { 
+            this.evaluate = true
+            var endResult = _.isEqual( this.status, this.oks.sort() )
+            if(endResult){
+                this.$emit('isright', true)
+                this.result = true
+            } else {
+                this.$emit('iswrong')
             }
         }
     },
     mounted () {
         //this.$emit('input', false)
         var _this = this
-        setTimeout(function (){
-            //_this.startConnections()
-        }, 1000)
+        jsPlumb.bind('connection',function(info,ev){
+            var con=info.connection;   //this is the new connection
+            var sourcedata = info.connection.source.getAttribute('data')
+            var targetid = info.connection.targetId
+            var allconnections = jsPlumb.getConnections()
+            var userConnections = []
+            for(con in allconnections){
+                const conn = [allconnections[con].sourceId, allconnections[con].targetId]
+                userConnections.push(conn)
+            }
+            _this.status = userConnections.sort()
+
+            _this.$emit('input', _this.status)
+
+            window.addEventListener("resize", function (){
+                console.log('resizing')
+                jsPlumb.repaintEverything()
+            })
+        })
     },
     
     template: `
-        <div class="relational row justify-content-between">
+        <div class="relational d-flex justify-content-between">
             <div class="relationalStart" @click="startConnections()" v-if="!started">
                 <img src="../../assets/aimg/relational.svg">
             </div>
-            <div class="sources col-4">
-                <div v-for="(source, index) in sources" :id="'s_'+index" :data="source.data">
+            
+            <div class="sources">
+                <div v-for="(source, index) in sources" class="r_source" :id="'s_'+index" :data="source.data">
                     <slot name="source" :source="source" />
                 </div>
             </div>
-            <div class="targets col-4">
-                <div v-for="(target, index) in targets" :id="'t_'+index"  :data="target.data">
+            <div class="targets">
+                <div v-for="(target, index) in targets" class="r_target" :id="'t_'+index" :data="target.data">
                     <slot name="target" :target="target" />
                 </div>
             </div>
